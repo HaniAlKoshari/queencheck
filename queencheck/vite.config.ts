a1bf657b96250229d6e2b25168e55f81080e5afb
HaniAlKoshari <142342020+HaniAlKoshari@users.noreply.github.com>
2026-01-19 00:47:45 +0300

    Add files via upload

--- queencheck/vite.config.ts
+++ queencheck/vite.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', 
+  base: './',
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
-  },
-  server: {
-    host: true,
-    port: 3000
+    assetsDir: 'assets',
+    emptyOutDir: true
   }
 });
