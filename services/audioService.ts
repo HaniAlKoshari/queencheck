eaa706f58953d8c21c9e7c766bc3ec2c4b9b34e3
HaniAlKoshari <142342020+HaniAlKoshari@users.noreply.github.com>
2026-01-19 19:05:06 +0300

    Update audioService.ts

--- services/audioService.ts
+++ services/audioService.ts
@@ -16,51 +16,55 @@ class AudioService {
 
   public playSuccess() {
     this.init();
+    if (!this.audioContext || this.audioContext.state === 'suspended') {
+      this.audioContext?.resume();
+    }
     if (!this.audioContext) return;
 
-    // اهتزاز خفيف للنجاح
     this.vibrate(50);
-
     const osc = this.audioContext.createOscillator();
     const gain = this.audioContext.createGain();
 
-    osc.type = 'sine'; // صوت ناعم
-    osc.frequency.setValueAtTime(660, this.audioContext.currentTime);
-    osc.frequency.exponentialRampToValueAtTime(880, this.audioContext.currentTime + 0.1);
-
+    osc.type = 'sine';
+    osc.frequency.setValueAtTime(880, this.audioContext.currentTime);
     gain.gain.setValueAtTime(0, this.audioContext.currentTime);
     gain.gain.linearRampToValueAtTime(0.2, this.audioContext.currentTime + 0.05);
-    gain.gain.exponentialRampToValueAtTime(0.01, this.audioContext.currentTime + 0.4);
+    gain.gain.exponentialRampToValueAtTime(0.01, this.audioContext.currentTime + 0.3);
 
     osc.connect(gain);
     gain.connect(this.audioContext.destination);
-
     osc.start();
-    osc.stop(this.audioContext.currentTime + 0.4);
+    osc.stop(this.audioContext.currentTime + 0.3);
   }
 
   public playError() {
     this.init();
+    if (!this.audioContext || this.audioContext.state === 'suspended') {
+      this.audioContext?.resume();
+    }
     if (!this.audioContext) return;
 
-    // اهتزاز قوي ومتكرر للخطأ
-    this.vibrate([200, 100, 200]);
+    // اهتزاز قوي ومكثف
+    this.vibrate([300, 100, 300, 100, 300]);
 
-    const osc = this.audioContext.createOscillator();
-    const gain = this.audioContext.createGain();
+    // نغمة Siren حادة
+    const playTone = (freq: number, start: number) => {
+      const osc = this.audioContext!.createOscillator();
+      const gain = this.audioContext!.createGain();
+      osc.type = 'sawtooth';
+      osc.frequency.setValueAtTime(freq, start);
+      osc.frequency.linearRampToValueAtTime(freq / 2, start + 0.4);
+      gain.gain.setValueAtTime(0.5, start);
+      gain.gain.linearRampToValueAtTime(0.01, start + 0.4);
+      osc.connect(gain);
+      gain.connect(this.audioContext!.destination);
+      osc.start(start);
+      osc.stop(start + 0.4);
+    };
 
-    osc.type = 'sawtooth'; // صوت حاد ومنبه
-    osc.frequency.setValueAtTime(120, this.audioContext.currentTime);
-    osc.frequency.linearRampToValueAtTime(60, this.audioContext.currentTime + 0.6);
-
-    gain.gain.setValueAtTime(0.4, this.audioContext.currentTime);
-    gain.gain.linearRampToValueAtTime(0.01, this.audioContext.currentTime + 0.6);
-
-    osc.connect(gain);
-    gain.connect(this.audioContext.destination);
-
-    osc.start();
-    osc.stop(this.audioContext.currentTime + 0.6);
+    playTone(440, this.audioContext.currentTime);
+    playTone(440, this.audioContext.currentTime + 0.2);
+    playTone(440, this.audioContext.currentTime + 0.4);
   }
 }
 
